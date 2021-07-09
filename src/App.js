9257efcc283ac8189b0e302a637b06751388060f
Suhas Bhairav <suhasbhairav@gmail.com>
2021-07-09 14:56:52 +0200

    Add new changes
    Add new changes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import React, {useState} from 'react';
 function App() {
   const [state, setstate] = useState("Suhas");
   const [number, setNumber] = useState(0);
-
+  const [age, setAge] = useState(10);
+  
   const changeValue = () => {
     setstate("Bhairav");
     setNumber(10);
   };
+
+  const increaseAge = () => {
+    setAge(20 * Math.random());
+  };
+
 
   return (
     <div className="App">
       <div>Hello {state}</div>
       <div>This is a {number}</div>
       <button onClick={changeValue}>Click</button>
+      <div>
+        Age is {age}
+        <button onClick={increaseAge}>Click Here</button>
+      </div>
     </div>
   );
 }
