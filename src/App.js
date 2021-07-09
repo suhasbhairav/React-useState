066066858c3b8fc12cf645b8d14ef5b6d72a4592
Suhas Bhairav <suhasbhairav@gmail.com>
2021-07-09 14:53:23 +0200

    Add initial changes
    Add initial changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [state, setstate] = useState("Suhas");
+  const [number, setNumber] = useState(0);
+
+  const changeValue = () => {
+    setstate("Bhairav");
+    setNumber(10);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Hello {state}</div>
+      <div>This is a {number}</div>
+      <button onClick={changeValue}>Click</button>
     </div>
   );
 }
